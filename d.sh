3a4306b8b90483a4360c129ae8fa16f587a1682a
bigstedi <104559553+bigstedi@users.noreply.github.com>
2022-04-28 14:39:16 +0530

    Add files via upload

---
+++ d.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir files_l
+cd files_l
+curl -o lab1_Linear_Regression.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/lab1_Linear_Regression.rmd
+curl -o lab2_time_series_forcasting.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/lab2_time_series_forcasting.rmd
+curl -o lab3_ARIMA_Weather.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/lab3_ARIMA_Weather.rmd
+curl -o lab4_ANNOVA.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/lab4_ANNOVA.rmd
+curl -o Lab5_Logistic_Regression.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/Lab5_Logistic_Regression.rmd
+curl -o Lab6_KNN.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/Lab6_KNN.rmd
+curl -o Lab7_Partioning_Based_Clustering.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/Lab7_Partioning_Based_Clustering.rmd
+curl -o Lab8_Hirearchical_Clustering.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/Lab8_Hirearchical_Clustering.rmd
+curl -o Lab9_Gradient_Descent.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/Lab9_Gradient_Descent.rmd
+curl -o lab10_Gradient_Descent_Momentum_Optimizer.rmd https://raw.githubusercontent.com/bigstedi/bigstedi/main/lab10_Gradient_Descent_Momentum_Optimizer.rmd
